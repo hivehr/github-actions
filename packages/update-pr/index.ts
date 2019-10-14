c359308cdaaaa0f71fd289379599046874870797
craig.thompson <craig.thompson@hive.hr>
2019-10-14 13:38:49 +0100

    chore: take into account WIP PR titles

--- packages/update-pr/index.ts
+++ packages/update-pr/index.ts
@@ -1,20 +1,36 @@
 import { getInput, debug, setFailed } from "@actions/core";
 import { GitHub, context } from "@actions/github";
 
+const {
+    repo: { owner, repo },
+    payload: { pull_request }
+} = context;
+
+const parseTitle = (oldTitle: string, title: string) => {
+    if (title == null || title === "") {
+        return undefined;
+    }
+
+    // Take care not to remove an WIP: prefixes
+    if (oldTitle.startsWith("WIP")) {
+        return `WIP: ${title}`;
+    }
+
+    return title;
+};
+
 async function main() {
-    const inputs = {
-        title: getInput("title")
-    };
-
-    debug(`title: ${inputs.title}`);
-
     const client = new GitHub(getInput("repo-token"));
 
+    // Parse new title
+    const title = parseTitle(pull_request!.title, getInput("title"));
+    debug(`title: ${title}`);
+
     await client.pulls.update({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        pull_number: context.payload.pull_request!.number,
-        title: inputs.title
+        owner,
+        repo,
+        title,
+        number: pull_request!.number
     });
 }
 
