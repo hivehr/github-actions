9f75c104aa4d907fc147dc20e51572933d6112de
craig.thompson <craig.thompson@hive.hr>
2019-10-14 15:35:56 +0100

    feat: addLabels and removeLabels support

--- packages/update-pr/index.ts
+++ packages/update-pr/index.ts
@@ -1,12 +1,7 @@
-import { getInput, debug, setFailed } from "@actions/core";
-import { GitHub, context } from "@actions/github";
+import { debug, getInput, setFailed } from "@actions/core";
+import { context, GitHub } from "@actions/github";
 
-const {
-    repo: { owner, repo },
-    payload: { pull_request }
-} = context;
-
-const parseTitle = (oldTitle: string, title: string) => {
+const parseTitle = (title: string, oldTitle: string) => {
     if (title == null || title === "") {
         return undefined;
     }
@@ -19,19 +14,66 @@ const parseTitle = (oldTitle: string, title: string) => {
     return title;
 };
 
+const parseLabels = (
+    addLabels: string,
+    removeLabels: string,
+    currentLabels: string[],
+    delimiter = ","
+) => {
+    if (
+        (addLabels == null || addLabels === "") &&
+        (removeLabels == null || removeLabels === "")
+    ) {
+        return undefined;
+    }
+
+    const labels = new Set([...currentLabels, ...addLabels.split(delimiter)]);
+    removeLabels.split(delimiter).forEach(l => labels.delete(l));
+
+    return Array.from(labels);
+};
+
+const parseInputs = (pull_request: any) => ({
+    title: parseTitle(getInput("title"), pull_request!.title),
+
+    labels: parseLabels(
+        getInput("addLabels"),
+        getInput("removeLabels"),
+        pull_request!.labels.map(({ name }: any) => name)
+    )
+});
+
 async function main() {
+    const {
+        repo: { owner, repo },
+        payload: { pull_request }
+    } = context;
+
+    // Grab and parse inputs
+    const inputs = parseInputs(pull_request);
+    debug(`Inputs: ${JSON.stringify(inputs)}`);
+
+    // Setup GitHub client with the given GITHUB_TOKEN
     const client = new GitHub(getInput("repo-token"));
 
-    // Parse new title
-    const title = parseTitle(pull_request!.title, getInput("title"));
-    debug(`title: ${title}`);
-
+    // Update PR with given inputs
     await client.pulls.update({
         owner,
         repo,
-        title,
+        title: inputs.title,
         pull_number: pull_request!.number
     });
+
+    // Update related PR issue with given labels
+    const { labels } = inputs;
+    if (labels != null) {
+        await client.issues.update({
+            owner,
+            repo,
+            labels,
+            number: pull_request!.number
+        });
+    }
 }
 
 if (require.main === module) {
