999849a7acaef587d55a5e784b4d699652199eaf
Gareth Lowrie <gareth.lowrie@hive.hr>
2019-11-11 22:55:58 +0700

    fix: testing

--- packages/dependency-checker/index.ts
+++ packages/dependency-checker/index.ts
@@ -1,5 +1,6 @@
 import { getInput, setFailed } from "@actions/core";
 import { context, GitHub } from "@actions/github";
+import { exec } from "child_process";
 
 async function main() {
   const {
@@ -9,23 +10,39 @@ async function main() {
 
   // 1. Setup GitHub client with the given GITHUB_TOKEN
   const client = new GitHub(getInput("repo-token"));
+  console.log("GITHUB_BASE_REF", process.env.GITHUB_BASE_REF);
+  console.log("GITHUB_HEAD_REF", process.env.GITHUB_HEAD_REF);
 
-  // 2. Check if any package.json files have changes
-  const files = await client.pulls.listFiles({
-    owner,
-    repo,
-    pull_number: pull_request!.number
-  });
+  exec(
+    `git diff ${process.env.GITHUB_BASE_REF} ${process.env.GITHUB_HEAD_REF}`,
+    (err, stdout, stderr) => {
+      if (err) {
+        // node couldn't execute the command
+        return;
+      }
 
-  // 3. Identify package.json files with changes
-  const packageJsonFiles = files.filter(
-    ({ filename, changes }) => filename === "package.json"
+      // the *entire* stdout and stderr (buffered)
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    }
   );
 
+  // 2. Check if any package.json files have changes
+  // const files = await client.pulls.listFiles({
+  //   owner,
+  //   repo,
+  //   pull_number: pull_request!.number
+  // });
+
+  // 3. Identify package.json files with changes
+  // const packageJsonFiles = files.filter(
+  //   ({ filename, changes }) => filename === "package.json"
+  // );
+
   // 4. If no changes - exit early
-  if (packageJsonFiles.length === 0) {
-    return;
-  }
+  // if (packageJsonFiles.length === 0) {
+  //   return;
+  // }
 
   // 5. Get a list of all the commits on the PR. (Do we care about this?)
   const commits = await client.pulls.listCommits({
@@ -34,19 +51,22 @@ async function main() {
     pull_number: pull_request!.number
   });
 
-  // 6. Locate the latest commit. (Do we care about this?)
-  const lastCommit = commits[commits.length - 1];
+  console.log(commits);
+
+  // // 6. Locate the latest commit. (Do we care about this?)
+  // const lastCommit = commits[commits.length - 1];
 
   // 7. Add a comment to the latest commit (Do we just add a comment to the PR instead?)
-  await client.pulls.createComment({
-    owner,
-    repo,
-    pull_number: pull_request!.number,
-    commit_id: lastCommit.sha,
-    path: "./package.json",
-    body: `Dependencys have changed\s\s
-    ${JSON.stringify(packageJsonFiles, null, 2)}`
-  });
+  // await client.pulls.createComment({
+  //   owner,
+  //   repo,
+  //   pull_number: pull_request!.number,
+  //   commit_id: lastCommit.sha,
+  //   position: 1,
+  //   path: "./package.json",
+  //   body: `Dependencys have changed\s\s`
+  //   // ${JSON.stringify(packageJsonFiles, null, 2)}`
+  // });
 }
 
 if (require.main === module) {
