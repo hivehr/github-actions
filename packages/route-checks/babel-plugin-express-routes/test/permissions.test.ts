303b0687612b9864c462a7651e549dce66705a5d
craig.thompson <craig.thompson@hive.hr>
2020-03-19 22:20:54 +0000

    feat: much improved tests using @babel/cli

--- packages/route-checks/babel-plugin-express-routes/test/permissions.test.ts
+++ packages/route-checks/babel-plugin-express-routes/test/permissions.test.ts
@@ -1,59 +1,63 @@
-import child_process from "child_process";
-import { promisify } from "util";
-import rimraf from "rimraf";
+import { transformFileSync } from "@babel/core";
 import fs from "fs";
+import { join, resolve } from "path";
+import rimraf from "rimraf";
 
-const exec = promisify(child_process.exec);
+const TESTS_DIR = __dirname;
+const BASE_DIR = resolve(__dirname, "..");
+const ROUTES_DIR = join(__dirname, "packages", "service", "src", "routes");
+const OUT_DIR = join(TESTS_DIR, "routes");
 
-const verifyFile = async (fileName: string) => {
-    const result = await exec(
-        `yarn babel --config-file ${process.cwd()}/test.babel.config.js --extensions .ts ${process.cwd()}/test/packages/service/src/routes/${fileName}.ts`
-    );
-    expect(result.stderr).toEqual("");
-    expect(
-        fs
-            .readFileSync(`${process.cwd()}/routes/service_${fileName}.json`)
-            .toString()
-    ).toMatchSnapshot();
+const transform = (filePath: string) =>
+    transformFileSync(filePath, {
+        presets: ["@babel/typescript"],
+        plugins: [
+            [
+                BASE_DIR,
+                {
+                    outPath: OUT_DIR,
+                    maxDepth: 10
+                }
+            ]
+        ]
+    });
+
+const verifyFile = (fileName: string) => {
+    const filePath = join(ROUTES_DIR, `${fileName}.ts`);
+    transform(filePath);
+
+    const resultFile = join(OUT_DIR, `service_${fileName}.json`);
+    expect(fs.readFileSync(resultFile).toString()).toMatchSnapshot();
 };
+
 describe("Permissions", () => {
     afterAll(() => {
         rimraf.sync(`${__dirname}/../routes/`);
     });
 
-    it("requireScope with single arg", async () => {
-        await verifyFile("require_scope_with_single_arg");
-    });
+    it("requireScope with single arg", () =>
+        verifyFile("require_scope_with_single_arg"));
 
-    it("requireScope with array arg", async () => {
-        await verifyFile("require_scope_with_array_arg");
-    });
+    it("requireScope with array arg", () =>
+        verifyFile("require_scope_with_array_arg"));
 
-    it("requireScope with single arg and 'or' with single arg", async () => {
-        await verifyFile(
-            "require_scope_with_single_arg_and_or_with_single_arg"
-        );
-    });
+    it("requireScope with single arg and 'or' with single arg", () =>
+        verifyFile("require_scope_with_single_arg_and_or_with_single_arg"));
 
-    it("requireScope with single arg and 'or' with array arg", async () => {
-        await verifyFile("require_scope_with_single_arg_and_or_with_array_arg");
-    });
+    it("requireScope with single arg and 'or' with array arg", () =>
+        verifyFile("require_scope_with_single_arg_and_or_with_array_arg"));
 
-    it("requireScope with array arg and 'or' with array arg", async () => {
-        await verifyFile("require_scope_with_array_arg_and_or_with_array_arg");
-    });
+    it("requireScope with array arg and 'or' with array arg", () =>
+        verifyFile("require_scope_with_array_arg_and_or_with_array_arg"));
 
-    it("route with no permissions required", async () => {
-        await verifyFile("no_permissions_required");
-    });
+    it("route with no permissions required", () =>
+        verifyFile("no_permissions_required"));
 
-    it("require scope with single rag and two ors with single arg", async () => {
-        await verifyFile(
+    it("require scope with single rag and two ors with single arg", () =>
+        verifyFile(
             "require_scope_with_single_rag_and_two_ors_with_single_arg"
-        );
-    });
+        ));
 
-    it("test all request methods", async () => {
-        await verifyFile("test_all_request_methods");
-    });
+    it("test all request methods", () =>
+        verifyFile("test_all_request_methods"));
 });
