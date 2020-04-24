2c4bbfdd1c45ed2af32d7cc9a8079efd0d5f776e
Gareth Lowrie <garethlowrie@gareths-mbp.home>
2020-04-24 22:20:27 +0100

    test

--- packages/extract-intl-messages/entrypoint.sh
+++ packages/extract-intl-messages/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eou pipefail
-yarn --cwd=/app babel --config-file /app/babel.config.js "${GITHUB_WORKSPACE}/${INPUT_PATH}"
+yarn --cwd=/app babel --config-file /app/babel.config.js "${GITHUB_WORKSPACE}/${INPUT_PATH}" 1> /dev/null
 
 
