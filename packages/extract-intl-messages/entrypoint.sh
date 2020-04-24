8b2aa5fa94a7d7f5f31464f39097d686c64ef290
Gareth Lowrie <garethlowrie@gareths-mbp.home>
2020-04-24 22:56:03 +0100

    t

--- packages/extract-intl-messages/entrypoint.sh
+++ packages/extract-intl-messages/entrypoint.sh
@@ -4,6 +4,7 @@ yarn --cwd=/app babel --config-file /app/babel.config.js "${GITHUB_WORKSPACE}/${
 yarn global add rimraf@2.6.2
 yarn global add ts-node@8.4.1
 
-yarn --cwd=/app/${GITHUB_WORKSPACE}/packages/app NODE_OPTIONS="--max-old-space-size=4096" ts-node -TP ${GITHUB_WORKSPACE}/packages/app/tsconfig.webpack.json ${GITHUB_WORKSPACE}/packages/app/scripts/intl/generate-messages-en.ts
-yarn --cwd=/app/${GITHUB_WORKSPACE}/packages/app rimraf ${GITHUB_WORKSPACE}/packages/app/build/intl/messages
+NODE_OPTIONS="--max-old-space-size=4096"
+yarn --cwd=/app/${GITHUB_WORKSPACE}/packages/app ts-node -TP tsconfig.webpack.json scripts/intl/generate-messages-en.ts
+yarn --cwd=/app/${GITHUB_WORKSPACE}/packages/app rimraf build/intl/messages
 
