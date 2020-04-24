5162d80a6c27b97e54e8ce29c95863638d623243
Gareth Lowrie <garethlowrie@gareths-mbp.home>
2020-04-24 23:22:35 +0100

    x

--- packages/extract-intl-messages/entrypoint.sh
+++ packages/extract-intl-messages/entrypoint.sh
@@ -5,6 +5,7 @@ yarn global add rimraf@2.6.2
 yarn global add ts-node@8.4.1
 
 NODE_OPTIONS="--max-old-space-size=4096"
+yarn --cwd=/app/${GITHUB_WORKSPACE}/packages/app
 yarn --cwd=/app/${GITHUB_WORKSPACE}/packages/app ts-node -TP ${GITHUB_WORKSPACE}/packages/app/tsconfig.webpack.json ${GITHUB_WORKSPACE}/packages/app/scripts/intl/generate-messages-en.ts
 yarn --cwd=/app/${GITHUB_WORKSPACE}/packages/app rimraf ${GITHUB_WORKSPACE}/packages/app/build/intl/messages
 
