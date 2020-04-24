df6aaf56496743a7f88b71c2914765903902f0d5
Gareth Lowrie <garethlowrie@gareths-mbp.home>
2020-04-24 22:43:59 +0100

    test

--- packages/extract-intl-messages/entrypoint.sh
+++ packages/extract-intl-messages/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eou pipefail
 yarn --cwd=/app babel --config-file /app/babel.config.js "${GITHUB_WORKSPACE}/${INPUT_PATH}" 1> /dev/null
-yarn --cwd=/app/${GITHUB_WORKSPACE}/packages/app yarn global add rimraf@2.6.2
-yarn --cwd=/app/${GITHUB_WORKSPACE}/packages/app yarn intl:postbuild
+yarn global add rimraf@2.6.2
+yarn --cwd=/app/${GITHUB_WORKSPACE}/packages/app intl:postbuild
 
