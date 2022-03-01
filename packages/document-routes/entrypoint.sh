d8b0d5e4b2c253dac4625c4ce86af2eec9c25b81
Scott Harwood <scott.harwood@hive.hr>
2022-03-01 20:21:58 +0000

    fix: disable glob expansion

--- packages/document-routes/entrypoint.sh
+++ packages/document-routes/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -eou pipefail
+set -efou pipefail
 yarn --cwd=/app generate:routes "${PWD}/${INPUT_PATTERN}" --ignore ${INPUT_IGNORE_PATTERNS} 1> /dev/null
 yarn --cwd=/app generate:md ${GITHUB_WORKSPACE}/${INPUT_OUT_PATH}/routes.md ${GITHUB_WORKSPACE}/${INPUT_OUT_PATH}
 
